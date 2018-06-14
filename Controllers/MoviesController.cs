eb2cdf7951ba2051c84ae1ff777d4af385d672ab
Stephanie <stephcshea@gmail.com>
2018-06-14 11:17:58 -0400

    inital commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using VidlyMosh.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VidlyMosh.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        //public ViewResult Random()
+        {
+            var movie = new Movie() { Name = "Chef" };
+
+            //inherited method from base Controller class
+            //Action Results examples
+            //could use return new ViewResult();
+            return View(movie);
+            //return Content("Good morning Abby!");
+            //return HttpNotFound(); -- didn't work
+            // return new EmptyResult(); -- blank page
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        //int? makes index nullable
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
